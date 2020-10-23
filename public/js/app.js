10c7b694bf0028fdbba8e5f63ad8ef4a3ba76f96
Nicola <nicolavalente88@gmail.com>
2020-10-23 16:29:20 +0200

    2: Animation Glitch

--- public/js/app.js
+++ public/js/app.js
@@ -37337,146 +37337,121 @@ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"
 
 $(function () {
   // Last Scroll Position
-  var lastScrollTop = 0; // On Scroll
+  var lastScrollPosition = $(document).scrollTop(); // Elements to animate
+
+  var glitchTop = $(".crop_icon.glitch_top");
+  var glitchBottom = $(".crop_icon.glitch_bottom"); // Animation time
+
+  var scrollAnimationDuration = 500; // Frames
+
+  var glitchTopBottomInitialPosition = -8;
+  var glitchTopScrollDownPosition = 12;
+  var glitchBottomScrollDownPosition = 2;
+  var glitchTopScrollUpPosition = -28;
+  var glitchBottomScrollUpPosition = -18;
+  var glitchInitialOpacity = 0;
+  var glitchScrollOpacity = 0.5; // GlitchTop state condition
+
+  var glitchTopState = parseInt(glitchTop.css("bottom"), 10); // GlitchBottom state condition
+
+  var glitchBottomState = parseInt(glitchBottom.css("bottom"), 10); // Support variable
+
+  var supportVariable = true; // On Scroll
 
   $(window).on('scroll', function () {
-    // Capture the full length of the page
-    var windowHeight = $(document).height(); // Capture where the top of the page is after scroll
+    // // Capture the full length of the page
+    // var windowHeight = $(document).height();
+    // Capture where the top of the page is after scroll
+    var currentPosition = $(document).scrollTop(); // // Capture how many pixels can be viewed by the user
+    // var windowViewingArea = $(window).height();
+    // // Figure out the bottom of what the user has scrolled to
+    // var bottomScrollPosition = currentPosition + windowViewingArea;
+    //
+    // // Figure out the rounded percentage of how much was scrolled
+    // var percentScrolled = parseInt((bottomScrollPosition / windowHeight * 100).toFixed(0));
+    // Animation -------------------------------------------------------------
+    // ScrollDown !!!
 
-    var currentPosition = $(document).scrollTop(); // Capture how many pixels can be viewed by the user
-
-    var windowViewingArea = $(window).height(); // Figure out the bottom of what the user has scrolled to
-
-    var bottomScrollPosition = currentPosition + windowViewingArea; // Figure out the rounded percentage of how much was scrolled
-
-    var percentScrolled = parseInt((bottomScrollPosition / windowHeight * 100).toFixed(0)); // Animation
-    // Elements to animate
-
-    var glitchTop = $(".glitch_top");
-    var glitchBottom = $(".glitch_bottom"); // Animation time
-
-    var scrollAnimationDuration = 300; // Scroll state
-
-    var scrollState = "scrollOn"; // Animation Condition
-    // -> If i !!! ScrollDown !!! the page
-
-    if (currentPosition > lastScrollTop && scrollState === 'scrollOn') {
-      // Start animation
-      // ->-> Element on TOP of the animation
-      // ->->-> Frame 1
-      console.log(scrollState);
+    if (lastScrollPosition - currentPosition < 0 && supportVariable === true) {
+      // Start Animatiom --->
+      // Glitch Top
+      // Frame Start
       glitchTop.animate({
-        bottom: [-8, "swing"],
-        opacity: [0, "swing"]
+        bottom: [glitchTopScrollDownPosition, "swing"],
+        opacity: [glitchScrollOpacity, "swing"]
       }, {
         duration: scrollAnimationDuration
-      }); // ->->-> Frame 2
+      }); // Frame End
 
       glitchTop.animate({
-        bottom: [12, "swing"],
-        opacity: [0.5, "swing"]
+        bottom: [glitchTopBottomInitialPosition, "swing"],
+        opacity: [glitchInitialOpacity, "swing"]
       }, {
         duration: scrollAnimationDuration
-      }); // ->->-> Frame 3
-
-      glitchTop.animate({
-        bottom: [-8, "swing"],
-        opacity: [0, "swing"]
-      }, {
-        duration: scrollAnimationDuration
-      }); // ->-> Element on BOTTOM of the animation
-      // ->->-> Frame 1
+      }); // Glitch Bottom
+      // Frame Start
 
       glitchBottom.animate({
-        bottom: [-8, "swing"],
-        opacity: [0, "swing"]
+        bottom: [glitchBottomScrollDownPosition, "swing"],
+        opacity: [glitchScrollOpacity, "swing"]
       }, {
         duration: scrollAnimationDuration
-      }); // ->->-> Frame 2
+      }); // Frame End
 
       glitchBottom.animate({
-        bottom: [2, "swing"],
-        opacity: [0.5, "swing"]
+        bottom: [glitchTopBottomInitialPosition, "swing"],
+        opacity: [glitchInitialOpacity, "swing"]
       }, {
         duration: scrollAnimationDuration
-      }); // ->->-> Frame 3
+      }); // Support Variable
 
-      glitchBottom.animate({
-        bottom: [-8, "swing"],
-        opacity: [0, "swing"]
-      }, {
-        duration: scrollAnimationDuration
-      }); // Change the scroll state
-
-      scrollState = 'scrollEnd';
-    } // -> If i !!! ScrollUp !!! the page
-    else if (currentPosition < lastScrollTop && scrollState === 'scrollOn') {
-        // Start animation
-        // ->-> Element on TOP of the animation
-        // ->->-> Frame 1
-        console.log(scrollState);
+      supportVariable = false;
+      console.log(supportVariable);
+    } // ScrollUp !!!
+    else if (lastScrollPosition - currentPosition > 0 && supportVariable === true) {
+        // Start Animatiom --->
+        // Glitch Top
+        // Frame Start
         glitchTop.animate({
-          bottom: [-8, "swing"],
-          opacity: [0, "swing"]
+          bottom: [glitchTopScrollUpPosition, "swing"],
+          opacity: [glitchScrollOpacity, "swing"]
         }, {
           duration: scrollAnimationDuration
-        }); // ->->-> Frame 2
+        }); // Frame End
 
         glitchTop.animate({
-          bottom: [-28, "swing"],
-          opacity: [0.5, "swing"]
+          bottom: [glitchTopBottomInitialPosition, "swing"],
+          opacity: [glitchInitialOpacity, "swing"]
         }, {
           duration: scrollAnimationDuration
-        }); // ->->-> Frame 3
-
-        glitchTop.animate({
-          bottom: [-8, "swing"],
-          opacity: [0, "swing"]
-        }, {
-          duration: scrollAnimationDuration
-        }); // ->-> Element on BOTTOM of the animation
-        // ->->-> Frame 1
+        }); // Glitch Bottom
+        // Frame Start
 
         glitchBottom.animate({
-          bottom: [-8, "swing"],
-          opacity: [0, "swing"]
+          bottom: [glitchBottomScrollUpPosition, "swing"],
+          opacity: [glitchScrollOpacity, "swing"]
         }, {
           duration: scrollAnimationDuration
-        }); // ->->-> Frame 2
+        }); // Frame End
 
+        console.log(glitchBottom.css("bottom"));
         glitchBottom.animate({
-          bottom: [-18, "swing"],
-          opacity: [0.5, "swing"]
+          bottom: [glitchTopBottomInitialPosition, "swing"],
+          opacity: [glitchInitialOpacity, "swing"]
         }, {
           duration: scrollAnimationDuration
-        }); // ->->-> Frame 3
+        });
+        console.log(glitchBottom.css("bottom")); // Support Variable
 
-        glitchBottom.animate({
-          bottom: [-8, "swing"],
-          opacity: [0, "swing"]
-        }, {
-          duration: scrollAnimationDuration
-        }); // Change the scroll state
-
-        scrollState = 'scrollEnd';
-      } // -> After scrolled
+        supportVariable = false;
+        console.log(supportVariable);
+      } // Update lastScrollPosition value with currentPosition value
 
 
-    if (scrollState === 'scrollEnd') {
-      // ->-> Stop animation
-      console.log(scrollState);
-      setTimeout(function () {
-        glitchTop.stop(true, false);
-      }, 900);
-      setTimeout(function () {
-        glitchBottom.stop(true, false);
-      }, 900); // Change the scroll state
-
-      scrollState = 'scrollOn';
-    } // Update last scroll position
-
-
-    lastScrollTop = currentPosition;
+    lastScrollPosition = currentPosition;
+    setTimeout(function () {
+      var supportVariable = true;
+    }, 500);
   }); // end On Scroll
 }); // end Scroll Glitch
 
--- resources/js/partials/ui/_animations.js
+++ resources/js/partials/ui/_animations.js
@@ -5,206 +5,165 @@ var $ = require( "jquery" );
 $(function() {
 
   // Last Scroll Position
-  var lastScrollTop = 0
+  var lastScrollPosition = $(document).scrollTop();
+
+  // Elements to animate
+  var glitchTop = $(".crop_icon.glitch_top");
+  var glitchBottom = $(".crop_icon.glitch_bottom");
+
+  // Animation time
+  var scrollAnimationDuration = 500;
+
+  // Frames
+  var glitchTopBottomInitialPosition = -8;
+  var glitchTopScrollDownPosition = 12;
+  var glitchBottomScrollDownPosition = 2;
+  var glitchTopScrollUpPosition = -28;
+  var glitchBottomScrollUpPosition = -18;
+  var glitchInitialOpacity = 0;
+  var glitchScrollOpacity = 0.5;
+
+  // GlitchTop state condition
+  var glitchTopState = parseInt(glitchTop.css("bottom"), 10);
+
+  // GlitchBottom state condition
+  var glitchBottomState = parseInt(glitchBottom.css("bottom"), 10);
+
+  // Support variable
+  var supportVariable = true;
 
   // On Scroll
   $(window).on('scroll', function() {
 
-      // Capture the full length of the page
-      var windowHeight = $(document).height();
+      // // Capture the full length of the page
+      // var windowHeight = $(document).height();
 
       // Capture where the top of the page is after scroll
       var currentPosition = $(document).scrollTop();
 
-      // Capture how many pixels can be viewed by the user
-      var windowViewingArea = $(window).height();
+      // // Capture how many pixels can be viewed by the user
+      // var windowViewingArea = $(window).height();
 
-      // Figure out the bottom of what the user has scrolled to
-      var bottomScrollPosition = currentPosition + windowViewingArea;
+      // // Figure out the bottom of what the user has scrolled to
+      // var bottomScrollPosition = currentPosition + windowViewingArea;
+      //
+      // // Figure out the rounded percentage of how much was scrolled
+      // var percentScrolled = parseInt((bottomScrollPosition / windowHeight * 100).toFixed(0));
 
-      // Figure out the rounded percentage of how much was scrolled
-      var percentScrolled = parseInt((bottomScrollPosition / windowHeight * 100).toFixed(0));
+      // Animation -------------------------------------------------------------
+      // ScrollDown !!!
+      if( (lastScrollPosition - currentPosition) < 0 && (supportVariable === true) ) {
 
-      // Animation
-
-      // Elements to animate
-      var glitchTop = $(".glitch_top");
-      var glitchBottom = $(".glitch_bottom");
-
-      // Animation time
-      var scrollAnimationDuration = 300;
-
-      // Scroll state
-      var scrollState = "scrollOn";
-
-      // Animation Condition
-      // -> If i !!! ScrollDown !!! the page
-      if( ( currentPosition > lastScrollTop ) && ( scrollState === 'scrollOn' ) ) {
-
-        // Start animation
-        // ->-> Element on TOP of the animation
-        // ->->-> Frame 1
-        console.log(scrollState)
+        // Start Animatiom --->
+        // Glitch Top
+        // Frame Start
         glitchTop.animate(
           {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
+            bottom: [glitchTopScrollDownPosition, "swing"],
+            opacity: [glitchScrollOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
-
-        // ->->-> Frame 2
+        // Frame End
         glitchTop.animate(
           {
-            bottom: [12, "swing"],
-            opacity: [0.5, "swing"],
+            bottom: [glitchTopBottomInitialPosition, "swing"],
+            opacity: [glitchInitialOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
 
-        // ->->-> Frame 3
-        glitchTop.animate(
+        // Glitch Bottom
+        // Frame Start
+        glitchBottom.animate(
           {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
+            bottom: [glitchBottomScrollDownPosition, "swing"],
+            opacity: [glitchScrollOpacity, "swing"],
+          },
+          {
+            duration: scrollAnimationDuration,
+          }
+        );
+        // Frame End
+        glitchBottom.animate(
+          {
+            bottom: [glitchTopBottomInitialPosition, "swing"],
+            opacity: [glitchInitialOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
 
-        // ->-> Element on BOTTOM of the animation
-        // ->->-> Frame 1
-        glitchBottom.animate(
-          {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
-          },
-          {
-            duration: scrollAnimationDuration,
-          }
-        );
-
-        // ->->-> Frame 2
-        glitchBottom.animate(
-          {
-            bottom: [2, "swing"],
-            opacity: [0.5, "swing"],
-          },
-          {
-            duration: scrollAnimationDuration,
-          },
-        );
-
-        // ->->-> Frame 3
-        glitchBottom.animate(
-          {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
-          },
-          {
-            duration: scrollAnimationDuration,
-          },
-        );
-
-          // Change the scroll state
-          scrollState = 'scrollEnd';
+        // Support Variable
+        supportVariable = false
+        console.log(supportVariable);
       }
 
-      // -> If i !!! ScrollUp !!! the page
-      else if( ( currentPosition < lastScrollTop) && ( scrollState === 'scrollOn' ) ) {
 
-        // Start animation
-        // ->-> Element on TOP of the animation
-        // ->->-> Frame 1
-        console.log(scrollState)
+      // ScrollUp !!!
+      else if( (lastScrollPosition - currentPosition) > 0 && (supportVariable === true) ) {
+
+        // Start Animatiom --->
+        // Glitch Top
+        // Frame Start
         glitchTop.animate(
           {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
+            bottom: [glitchTopScrollUpPosition, "swing"],
+            opacity: [glitchScrollOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
-
-        // ->->-> Frame 2
+        // Frame End
         glitchTop.animate(
           {
-            bottom: [-28, "swing"],
-            opacity: [0.5, "swing"],
+            bottom: [glitchTopBottomInitialPosition, "swing"],
+            opacity: [glitchInitialOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
 
-        // ->->-> Frame 3
-        glitchTop.animate(
+        // Glitch Bottom
+        // Frame Start
+        glitchBottom.animate(
           {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
+            bottom: [glitchBottomScrollUpPosition, "swing"],
+            opacity: [glitchScrollOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
-
-        // ->-> Element on BOTTOM of the animation
-        // ->->-> Frame 1
+        // Frame End
+        console.log(glitchBottom.css("bottom"));
         glitchBottom.animate(
           {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
+            bottom: [glitchTopBottomInitialPosition, "swing"],
+            opacity: [glitchInitialOpacity, "swing"],
           },
           {
             duration: scrollAnimationDuration,
           }
         );
+        console.log(glitchBottom.css("bottom"));
 
-        // ->->-> Frame 2
-        glitchBottom.animate(
-          {
-            bottom: [-18, "swing"],
-            opacity: [0.5, "swing"],
-          },
-          {
-            duration: scrollAnimationDuration,
-          },
-        );
-
-        // ->->-> Frame 3
-        glitchBottom.animate(
-          {
-            bottom: [-8, "swing"],
-            opacity: [0, "swing"],
-          },
-          {
-            duration: scrollAnimationDuration,
-          },
-        );
-
-          // Change the scroll state
-          scrollState = 'scrollEnd';
+        // Support Variable
+        supportVariable = false
+        console.log(supportVariable);
       }
 
-      // -> After scrolled
-      if( scrollState === 'scrollEnd' ) {
-        // ->-> Stop animation
-        console.log(scrollState)
-        setTimeout(function(){ glitchTop.stop(true, false); }, 900);
-        setTimeout(function(){ glitchBottom.stop(true, false); }, 900);
+      // Update lastScrollPosition value with currentPosition value
+      lastScrollPosition = currentPosition;
 
-        // Change the scroll state
-        scrollState = 'scrollOn';
-      }
-
-      // Update last scroll position
-      lastScrollTop = currentPosition;
+      setTimeout(function(){ var supportVariable = true }, 500);
   });
   // end On Scroll
 
