677e67d5db6721cddd6efe17415a8d1ba2f9b771
Nicola <nicolavalente88@gmail.com>
2020-10-24 12:44:11 +0200

    6: Footer

--- resources/views/partials/structure/footer.blade.php
+++ resources/views/partials/structure/footer.blade.php
@@ -2,20 +2,36 @@
 
 {{-- Footer --}}
 <footer>
+
+  {{-- Ui interface --}}
   <div class="ui">
+
+    {{-- Crop marks --}}
     <div class="crop_marks d-flex justify-content-between">
+
+      {{-- Left crop mark --}}
       <div class="corner left d-flex justify-content-end align-items-center">
         <span id="animation" class="crop_icon glitch glitch_top">+</span>
         <span id="animation" class="crop_icon glitch glitch_bottom">+</span>
         <span id="animation" class="crop_icon main_icon">+</span>
       </div>
+      {{-- end Left crop mark --}}
+
+      {{-- Right crop mark --}}
       <div class="corner right d-flex justify-content-start align-items-center">
         <span id="animation" class="crop_icon glitch glitch_top">+</span>
         <span id="animation" class="crop_icon glitch glitch_bottom">+</span>
         <span id="animation" class="crop_icon main_icon">+</span>
       </div>
+      {{-- end Right crop mark --}}
+
     </div>
+    {{-- end Crop marks --}}
+
+    {{-- Bottom ui --}}
     <div class="bottom_ui d-flex justify-content-between align-items-start">
+
+      {{-- Square colors --}}
       <div class="colors_palette d-flex justify-content-start">
         <span class="square color_1"></span>
         <span class="square color_2"></span>
@@ -23,10 +39,45 @@
         <span class="square color_4"></span>
         <span class="square color_5"></span>
       </div>
-      <div class="d-flex align-items-start">
-        <span>Info sul mio portfolio</span>
+      {{-- end Square colors --}}
+
+      {{-- Ui info --}}
+      <div class="ui_info">
+        <ul class="socials_list d-flex">
+
+          {{-- Git --}}
+          <li class="git">
+            <a href="https://github.com/malimbo88">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          {{-- Linkedin --}}
+          <li class="linkedin">
+            <a href="https://www.linkedin.com/in/nicola-valente-339a0010a/">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </li>
+          {{-- Behance --}}
+          <li class="linkedin">
+            <a href="https://www.behance.net/malimbo">
+              <i class="fab fa-behance"></i>
+            </a>
+          </li>
+          {{-- Mail --}}
+          <li class="linkedin">
+            <a href="https://www.gmail.com">
+              <i class="fas fa-at"></i>
+            </a>
+          </li>
+        </ul>
       </div>
+      {{-- end Ui info --}}
+
     </div>
+    {{-- end Bottom ui --}}
+
   </div>
+  {{-- end Ui interface --}}
+
 </footer>
 {{-- end Footer --}}
--- resources/views/partials/structure/header.blade.php
+++ resources/views/partials/structure/header.blade.php
@@ -2,11 +2,20 @@
 
 {{-- Header --}}
 <header>
+
+  {{-- Ui interface --}}
   <div class="ui">
+
+    {{-- Top ui --}}
     <div class="top_ui d-flex justify-content-between align-items-end">
-      <div class="d-flex align-items-end">
+
+      {{-- Ui info --}}
+      <div class="ui_info">
         <span>Info sul mio portfolio</span>
       </div>
+      {{-- end Ui info --}}
+
+      {{-- Square colors --}}
       <div class="colors_palette d-flex justify-content-end">
         <span class="square color_1"></span>
         <span class="square color_2"></span>
@@ -14,19 +23,35 @@
         <span class="square color_4"></span>
         <span class="square color_5"></span>
       </div>
+      {{-- Square colors --}}
+
     </div>
+    {{-- end Top ui --}}
+
+    {{-- Crop marks --}}
     <div class="crop_marks d-flex justify-content-between">
+
+      {{-- Left crop mark --}}
       <div class="corner left d-flex justify-content-end align-items-center">
         <span id="animation" class="crop_icon glitch glitch_top">+</span>
         <span id="animation" class="crop_icon glitch glitch_bottom">+</span>
         <span id="animation" class="crop_icon main_icon">+</span>
       </div>
+      {{-- end Left crop mark --}}
+
+      {{-- Right crop mark --}}
       <div class="corner right d-flex justify-content-start align-items-center">
         <span id="animation" class="crop_icon glitch glitch_top">+</span>
         <span id="animation" class="crop_icon glitch glitch_bottom">+</span>
         <span id="animation" class="crop_icon main_icon">+</span>
       </div>
+      {{-- end Right crop mark --}}
+      
     </div>
+    {{-- end Crop marks --}}
+
   </div>
+  {{-- end Ui interface --}}
+
 </header>
 {{-- end Header --}}
